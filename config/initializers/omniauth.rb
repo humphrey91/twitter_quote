a1f90d770b1f76549ad275440dbd34f3d77265a7
Josh Humphrey <humphrey.js@gmail.com>
2017-06-06 14:03:58 +0700

    fix oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, Rails.application.secrets.twitter_api_key, Rails.application.secrets.twitter_api_secret
+  provider :twitter, ENV['twitter_api_key'], ENV['twitter_api_secret']
 end
